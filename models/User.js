1568275551e7164d0dd71eb3bb092eb248ab522c
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-06-05 14:40:00 +0200

    Add auth controller logic part I

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Sechema({
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Please provide a username']
@@ -23,6 +24,20 @@ const UserSchema = new mongoose.Sechema({
     resetPasswordToken: String,
     resetPasswordExpire: Date
 });
+
+UserSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+UserSchema.methods.matchPasswords = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 const User = mongoose.model("User", UserSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config({path: "./config.env"});
 const express = require('express');
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
 
 const app = express();
 app.use(express.json());
@@ -7,4 +11,9 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(err);
+    server.close(() => process.exit(1));
+})
