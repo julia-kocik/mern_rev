613d99cdde13837b7814a273211bd057748b3fee
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-06-12 12:10:07 +0200

    Setup emailing part II

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require('../utils/sendEmail');
+const crypto = require("crypto");
+
 
 exports.register = async (req, res, next) => {
 
@@ -16,10 +18,7 @@ exports.register = async (req, res, next) => {
         sendToken(user, 201, res)
 
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        })
+        next(error);
     }
    
 }
@@ -28,25 +27,24 @@ exports.login = async (req, res, next) => {
     const { email, password } = req.body;
 
     if(!email || !password) {
-        res.status(400).json({ success: false, error: "Please provide email and password"})
-    }
+        return next(new ErrorResponse("Please provide an email and password", 400));    }
 
     try {
         const user = await User.findOne({ email }).select("+password");
 
         if(!user) {
-            res.status(404).json({ success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Invalid credentials", 401));
         }
 
         const isMatch = await user.matchPasswords(password);
 
         if(!isMatch) {
-            res.status(404).json({ success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Invalid credentials", 401));
         }
 
-        sendToken(user, 201, res)
+        sendToken(user, 200, res)
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        next(error);
     }
 }
 
@@ -91,8 +89,29 @@ exports.forgotpassword = async (req, res, next) => {
     }
 }
 
-exports.resetpassword = (req, res, next) => {
-    res.send("Reset password Route");
+exports.resetpassword = async (req, res, next) => {
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.resetToken).digest("hex");
+    try {
+        const user = await User.findOne({
+            resetPasswordToken: resetPasswordToken,
+            resetPasswordExpire: { $gt: Date.now()}
+        })
+
+        if(!user) {
+            return next(new ErrorResponse("Invalid Reset Token", 400))
+        }
+            user.password = req.body.password;
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+        
+            await user.save();
+        res.status(201).json({
+            success: true,
+            data: "Password Reset Success"
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
 const sendToken = (user, statusCode, res) => {
