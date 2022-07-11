bfd2d2d6b30f142a8cca895ed7a7daf3d7bd6796
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-11 21:56:31 +0200

    Add Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Routing
+import PrivateRoute from "./components/routing/PrivateRoute";
+
+// Screens
+import PrivateScreen from "./components/screens/PrivateScreen";
+import { LoginScreen } from './components/screens/LoginScreen';
+import { RegisterScreen } from './components/screens/RegisterScreen';
+import { ForgotPasswordScreen} from './components/screens/ForgotPasswordScreen';
+import { ResetPasswordScreen } from './components/screens/ResetPasswordScreen';
 
 const App = () => {
   return (
-    <div className="App">
-      React App      
-    </div>
+   <Router>
+     <div className="app">
+      <Routes>
+        <PrivateRoute exact path="/" element={<PrivateScreen/>} />
+        <Route exact path="/login" element={<LoginScreen/>}/>
+        <Route exact path="/register" element={<RegisterScreen/>}/>
+        <Route exact path="/forgotpassword" element={<ForgotPasswordScreen/>}/>
+        <Route exact path="/passwordreset/:resetToken" element={<ResetPasswordScreen/>}/>
+      </Routes>
+     </div>
+   </Router>
   );
 }
 
