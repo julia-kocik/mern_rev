cc02e12677a64e08edb15234f368dc42f3b14820
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-29 22:22:27 +0200

    Adjust Worksession model

--- models/Worksession.js
+++ models/Worksession.js
@@ -1,9 +1,11 @@
-const Worksession = {
+const mongoose = require('mongoose');
+
+const Worksession = new mongoose.Schema({
     name: String,
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     duration: { type: Number, required: true },
-};
+});
 
 
 module.exports = Worksession;
