b9caa4807b7140dc415300ce1e11ae5777590aa6
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-30 19:40:29 +0200

    Add loading to PrivateScreen page

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 function PrivateScreen() {
   const [error, setError] = useState("");
   const [privateData, setPrivateData] = useState("");
-
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     if(!localStorage.getItem("authToken")) {
@@ -23,8 +23,10 @@ function PrivateScreen() {
 
       try {
         const {data} = await axios.get("/api/private", config);
-        setPrivateData(data.data)
+        setLoading(false);
+        setPrivateData(data)
       } catch (error) {
+        setLoading(false);
         localStorage.removeItem("authToken");
         setError("You are not authorized, please login")
         console.log(error)
@@ -41,9 +43,15 @@ function PrivateScreen() {
 
   return error ? (
      <span className="error-message">{error}</span>
+  ) : loading ? (
+    <span className="error-message">Loading...</span>
+
   ) : (
     <>
-      <div style={{ background: "green", color: "white"}}>{privateData}</div>
+      <div style={{ background: "green", color: "white"}}>{privateData.data}</div>
+      <div>
+        <h1>{`Hello ${privateData.user.username[0].toUpperCase()}${privateData.user.username.slice(1)}`}</h1>
+      </div>
       <button onClick={logoutHandler}>Logout</button>
     </>
   )
