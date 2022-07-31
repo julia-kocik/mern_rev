620071b4bfb1596f5e0231f20881ae1cac506f95
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-31 12:57:35 +0200

    Add Clock component

---
+++ client/src/components/layout/Clock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Clock.css';
+
+const Clock = () => {
+  return (
+    <div className='clock__container'>
+        <button className='clock__button'>Start</button>
+    </div>
+  )
+}
+
+export default Clock
