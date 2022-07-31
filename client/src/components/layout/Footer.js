5dc6ca174ebec2a797cfadf9c901e12b41efda67
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-31 11:47:43 +0200

    Add Footer component

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer__container'>
+        <span className='footer__span'>Julia Kocik 2022</span>
+    </div>
+  )
+}
+
+export default Footer
