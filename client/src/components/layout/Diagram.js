8e024d0c8dcd9505bc5b5f6b7e5ed14670f8d0a8
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-31 11:10:57 +0200

    Add Diagram component

---
+++ client/src/components/layout/Diagram.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Diagram.css';
+
+const Diagram = () => {
+  return (
+    <div className='diagram__container'>
+        <h2 className='diagram__section__title'>Diagram</h2>
+    </div>
+  )
+}
+
+export default Diagram
