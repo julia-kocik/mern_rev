21df6056f52148d6dc0f6072baf4c38f49960035
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-07-31 15:06:59 +0200

    Add Requests component

---
+++ client/src/components/layout/Requests.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Requests.css'
+
+const Requests = () => {
+  return (
+    <div className='requests__container'>
+        <button className='requests__button__disabled'>Save</button>
+    </div>
+  )
+}
+
+export default Requests
