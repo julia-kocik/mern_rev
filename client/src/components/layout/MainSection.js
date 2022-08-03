dbf15f454dea88b379422e4069d7b35ee103ac7c
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-03 21:57:50 +0200

    Add name and date to state in MainSection

--- client/src/components/layout/MainSection.js
+++ client/src/components/layout/MainSection.js
@@ -1,22 +1,31 @@
 import React, {useState} from 'react'
 import "./MainSection.css"
 import Sessions from './Sessions';
-import Diagram from './Diagram';
 import Clock from './Clock';
 import Requests from './Requests';
+import {getDate} from '../../utils/utils';
 
 const MainSection = props => {
 const {sessions} = props;
 const [status, setStatus] = useState('initial');
 const [counter, setCounter] = useState(0);
+const [name, setName] = useState('');
+const [date, setDate] = useState(null);
 const establishStatus = () => {
   if(status === 'initial') {
-    setStatus('ongoing')
+    setStatus('ongoing');
   } else if(status==='ongoing') {
+    if(name==='') {
+      const sessionName = prompt("Please enter session name");
+      setName(sessionName)
+    }
+    setDate(getDate())
     setStatus('stopped')
   } else if(status==='stopped') {
-    setStatus('initial')
+    setStatus('initial');
     setCounter(0);
+    setName('');
+    setDate(null)
   }
 }
 const startCounter = () => {
@@ -31,12 +40,11 @@ const startCounter = () => {
                 <Clock status={status} establishStatus={establishStatus} counter={counter} startCounter={startCounter}/>
             </div>
             <div className='mainsection__requests'>
-                <Requests/>
+                <Requests name={name} date={date} counter={counter} status={status}/>
             </div>
         </div>
         <div className='mainsection__bottom'>
               <Sessions sessions={sessions}/>
-              <Diagram/>
         </div>
     </div>
   )
