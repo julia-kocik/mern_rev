6e5b50ee46d9bc24399c57f5051b5975c1bc27ac
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-05 20:59:37 +0200

    Add DeleteOne handler

--- client/src/components/layout/Sessions.js
+++ client/src/components/layout/Sessions.js
@@ -2,15 +2,34 @@ import React from 'react';
 import "./Sessions.css";
 import {formatTime} from '../../utils/utils';
 import { AiFillDelete } from "react-icons/ai";
+import axios from 'axios';
 
-const Sessions = props => {
-  const {sessions} = props;
+const Sessions = ({sessions}) => {
+    const handleDeleteOne = async (id) => {
+          try {
+            const config = {
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("authToken")}`
+              }
+            }
+            const { data } = await axios.delete(
+              `/api/private/sessions/${id}`,
+              config
+            );
+            console.log(data);
+            alert('Session successfully deleted');
+          } catch (error) {
+            console.log(error)
+            alert(error)
+          }
+      }
   return (
     <div className='sessions__container'>
         <h2 className='sessions__section__title'>Sessions</h2> 
         {sessions.length  
         ? sessions.map(item => (
-            <div className='sessions__session__container'>
+            <div key={item._id} className='sessions__session__container'>
                 <div className='sessions__session__item'>
                     <h5>{item.name ? `${item.name[0].toUpperCase()}${item.name.slice(1)}` : 'Unnamed session'}</h5>
                 </div>
@@ -21,7 +40,9 @@ const Sessions = props => {
                     <p>Duration: {formatTime(item.counter)}</p>
                 </div>
                 <div className='sessions__session__item__last'>                
-                    <p className='sessions__session__item__icon'><AiFillDelete/></p>
+                    <p className='sessions__session__item__icon' onClick={() => {
+                        handleDeleteOne(item._id);
+                    }}><AiFillDelete/></p>
                 </div>
             </div>
         )) 
