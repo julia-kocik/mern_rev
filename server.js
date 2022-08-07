d0c1c75862857bee8584cb6eb8cfd7a5daed20a7
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-07 15:33:22 +0200

    Try to fix deploy issue

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('*', (req, res) => {
 });
   
 
-const server = app.listen(PORT || 5000, () => console.log(`Server is running on ${PORT}`));
+const server = app.listen(process.env.PORT || 5000, () => console.log(`Server is running on ${PORT}`));
 
 process.on("unhandledRejection", (err, promise) => {
     console.log(err);
